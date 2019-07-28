91a36746964aefeb7811753eb678d93fb77c0bf6
NicholasDollick <nidollick@gmail.com>
2019-07-28 02:12:34 -0700

    added register component. Started work on styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { appRoutes } from './routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      LoginComponent
+      LoginComponent,
+      RegisterComponent,
+      HomeComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      RecaptchaModule.forRoot(),
+      FormsModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule
    ],
    providers: [],
    bootstrap: [
---
+++ src/app/routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
+
+export const appRoutes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'login', component: LoginComponent, data: { animation: 'isLeft'}},
+    {path: 'register', component: RegisterComponent, data: { animation: 'isRight'}},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+
+];
