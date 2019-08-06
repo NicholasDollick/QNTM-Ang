62f307f91508bbe407d055565664edd1561c0ee5
NicholasDollick <nidollick@gmail.com>
2019-08-05 19:05:38 -0700

    semi working

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -23,16 +23,33 @@ export class TestComponent implements OnInit {
     }).catch(err => {
       console.log('Error starting connection');
     });
+    /*
     this.hubConnection.on('sendToAll', (name: string, message: string) => {
       const text = `${name}: ${message}`;
-      this.messages.push(text);
+      if (name !== this.name) {
+        console.log('this wasnt my message');
+      } else {
+        console.log('this was my message');
+      }
+      const test = JSON.stringify({username: name, msg: message, sentByMe: true});
+      this.messages.push(JSON.parse(test));
+    });
+
+    */
+    this.hubConnection.on('chatMessages', (data: string) => {
+      console.log(JSON.parse(data));
+      this.messages.push(JSON.parse(data));
     });
   }
 
-sendMessage() {
+sendMessage1() {
   this.hubConnection.invoke('sendToAll', this.name, this.message).catch(err => {
     console.error(err);
   });
+}
+
+sendMessage() {
+  this.hubConnection.invoke('chatMessages', JSON.stringify({username: this.name, msg: this.message, sentByMe: true}));
 }
 
 }
